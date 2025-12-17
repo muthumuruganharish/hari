496a5a4bde3d4fc62085854aa3a943694f2cf095
harish <muthumuruganharish272003@gmail.com>
2025-12-17 06:19:54 +0530

    basic java program added

---
+++ OneDrive/Desktop/Java Full Stack/TNS_java/src/com/tnsif/basic/SecondProject.java
@@ -0,0 +1,17 @@
+package com.tnsif.basic;
+
+public class SecondProject {
+
+	public static void main(String[] args) {
+		int beg=10;
+		int end=20;
+		for(int j=beg; j<=end;j++) {
+		for (int i=0;i<10;i++) {
+			
+			System.out.println(i);
+		}
+		System.out.println();
+		}
+	}
+
+}
